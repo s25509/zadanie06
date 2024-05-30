923a7ce63d5650b666957e6dfd7c28ce0ee14ac9
s25509 <s25509@pjwstk.edu.pl>
2024-05-30 14:18:45 +0200

    added first endpoint

---
+++ EFC_Code_First/Controllers/PatientController.cs
@@ -0,0 +1,61 @@
+﻿using EFC_Code_First.Context;
+using EFC_Code_First.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFC_Code_First.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class PatientController(AppDbContext context) : ControllerBase
+{
+    [HttpGet("{idPatient:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetPatientWithAllDataAsync(int idPatient)
+    {
+        var patient = await context.Patients
+            .Where(p => p.IdPatient == idPatient)
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.IdDoctorNavigation)
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.PrescriptionMedicaments)
+            .ThenInclude(pm => pm.IdMedicamentNavigation)
+            .Select(p => new PatientDto
+            {
+                IdPatient = p.IdPatient,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                Birthdate = p.Birthdate,
+                Prescriptions = p.Prescriptions
+                    .OrderByDescending(pr => pr.DueDate)
+                    .Select(pr => new PrescriptionDto
+                    {
+                        IdPrescription = pr.IdPrescription,
+                        Date = pr.Date,
+                        DueDate = pr.DueDate,
+                        IdDoctorNavigation = new DoctorDto
+                        {
+                            IdDoctor = pr.IdDoctorNavigation.IdDoctor,
+                            FirstName = pr.IdDoctorNavigation.FirstName,
+                            LastName = pr.IdDoctorNavigation.LastName,
+                            Email = pr.IdDoctorNavigation.Email
+                        },
+                        Medicaments = pr.PrescriptionMedicaments.Select(pm => new MedicamentDto
+                            {
+                                IdMedicament = pm.IdMedicamentNavigation.IdMedicament,
+                                Name = pm.IdMedicamentNavigation.Name,
+                                Description = pm.IdMedicamentNavigation.Description,
+                                Type = pm.IdMedicamentNavigation.Type
+                            }
+                        ).ToList()
+                    }
+                ).ToList()
+            })
+            .FirstOrDefaultAsync();
+
+        if (patient is null) return NotFound($"Patient with ID: {idPatient} not found");
+
+        return Ok(patient);
+    }
+}
---
+++ EFC_Code_First/DTOs/PrescriptionsDTOs.cs
@@ -0,0 +1,36 @@
+﻿namespace EFC_Code_First.Models
+{
+    public class PatientDto
+    {
+        public int IdPatient { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly Birthdate { get; set; }
+        public List<PrescriptionDto> Prescriptions { get; set; }
+    }
+
+    public class PrescriptionDto
+    {
+        public int IdPrescription { get; set; }
+        public DateOnly Date { get; set; }
+        public DateOnly DueDate { get; set; }
+        public DoctorDto IdDoctorNavigation { get; set; }
+        public List<MedicamentDto> Medicaments { get; set; }
+    }
+
+    public class DoctorDto
+    {
+        public int IdDoctor { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class MedicamentDto
+    {
+        public int IdMedicament { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
