4b26e6e5c67270dd3e7f7e1c8ca164f386ea926e
s25509 <s25509@pjwstk.edu.pl>
2024-05-30 10:37:46 +0200

    got connection to DB

---
+++ EFC_Code_First/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFC_Code_First.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext() {}
+    
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       
+    }
+}
