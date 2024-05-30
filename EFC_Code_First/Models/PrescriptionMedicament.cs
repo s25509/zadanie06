26b033c4e3bdbc2020ad78d7a7cffbec4c7f3348
s25509 <s25509@pjwstk.edu.pl>
2024-05-30 10:58:51 +0200

    small oversight

--- EFC_Code_First/Models/PrescriptionMedicament.cs
+++ EFC_Code_First/Models/PrescriptionMedicament.cs
@@ -4,7 +4,9 @@ public class PrescriptionMedicament
 {
     public int IdMedicament { get; set; }
     public int IdPrescription { get; set; }
-    
+    public int ?Dose { get; set; }
+    public string Details { get; set; }
+
     public virtual Prescription IdPrescriptionNavigation { get; set; }
     public virtual Medicament IdMedicamentNavigation { get; set; }
 }
